4230c8f1d217a9b08a5d6c13814e857ad74b4e08
luanaassis <luanaassis0908@gmail.com>
2023-07-13 21:34:23 -0300

    Passo 2

--- MoneyTest.java
+++ MoneyTest.java
@@ -1,21 +1,15 @@
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class DollarTest {
-    @Test
-    public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
-    }
-
+public class MoneyTest {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 
 }
